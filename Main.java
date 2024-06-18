76e74ced1701d26e22afe1ef63c3337287becd8f
punba1228 <68808553+punba1228@users.noreply.github.com>
2024-06-19 08:43:55 +0900

    lesson0-3

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("すがわら");
+    // エラーが発生するコード
+    // System.out.println("湊くんかっこいい！最高！);
+    System.out.println("湊くんかっこいい！最高！");
   }
 }
