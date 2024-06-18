71cf35bd89fd56a80e1a2ea76caf6cd306d71892
punba1228 <68808553+punba1228@users.noreply.github.com>
2024-06-19 08:45:57 +0900

    lesson0-4

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    // エラーが発生するコード
-    // System.out.println("湊くんかっこいい！最高！);
-    System.out.println("湊くんかっこいい！最高！");
+    System.out.println("すがわら");
+    System.out.println("３１歳です");
+    System.out.println("お酒が好きです");
   }
 }
