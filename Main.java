3c29cc6d14e5129a24cde245cbe2469d6baef8d9
punba1228 <68808553+punba1228@users.noreply.github.com>
2024-08-13 08:43:00 +0900

    test1-1-2

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 // 円周率に定数を利用する（エラー）
 public class Main {
   public static void main(String[] args) {
-    // 定数として円周率を宣言
-    final double pi = 3.14;
-    int pie = 5;
-    System.out.println("半径" + pie + "cmのパイの面積は、" );
-    System.out.println(pie * pie * pi);
-    System.out.println("パイの半径を倍にします");
-    // コンパイルエラーになる
-    pi = 10;
-    System.out.println("半径" + pie + "cmのパイの面積は、" );
-    System.out.println(pie * pie * pi);
+    int a;
+    int b;
+    int c;
+
+    a = 3;
+    b = 5;
+    c = a * b;
+    System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
   }
 }
