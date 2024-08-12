1857dc5d77695a51307faeab2d3d24d4d3671441
punba1228 <68808553+punba1228@users.noreply.github.com>
2024-08-13 08:01:33 +0900

    lesson1-1

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("すがわら");
-    System.out.println("３１歳です");
-    System.out.println("お酒が好きです");
-    System.out.println("31 + 31の計算をします");
-    System.out.println(31+31);
-    int x;
-    x = 6;
-    System.out.println(x * x * 3.14);
+    System.out.println("RPG:スッキリ魔王征伐");
+    System.out.println("Ver.0.1 by 湊");
+    System.out.println("＜ただいま鋭意学習・制作中＞");
+    System.out.println("プログラムを終了します");
   }
 }
