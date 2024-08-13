d145a2444e17d023c6bcb3abf7d577ee82951715
punba1228 <68808553+punba1228@users.noreply.github.com>
2024-08-14 08:31:22 +0900

    lesson2-1

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 // 円周率に定数を利用する（エラー）
 public class Main {
   public static void main(String[] args) {
-    boolean result = true;
-    char favoriteCharacter = '駆';
-    double pi = 3.14;
-    long number = 314159265853979L;
-    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    int a;
+    int b;
+    a = 20;
+    b = a + 5;
+    System.out.println(a);
+    System.out.println(b);
   }
 }
