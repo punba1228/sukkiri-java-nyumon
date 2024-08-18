587fd344612d1ee129aa5062517b97a6e44bb5af
punba1228 <68808553+punba1228@users.noreply.github.com>
2024-08-18 12:10:45 +0900

    lesson2-3

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-// エスケープシーケンスを用いていない例（エラー）
+// エスケープシーケンスを用いた例
 public class Main {
   public static void main(String[] args) {
-    System.out.println("私の好きな記号は二重引用符（"）です");
+    System.out.println("私の好きな記号は二重引用符（\"）です");
   }
 }
