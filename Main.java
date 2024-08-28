68b7f5356e7c828f0f06287e3b2e378f16d50580
punba1228 <68808553+punba1228@users.noreply.github.com>
2024-08-29 08:51:50 +0900

    lesson2-7

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-// データより小さな型の変数に代入（エラー）
+// 強制的な型変換
 public class Main {
   public static void main(String[] args) {
-    // 小数点はどうなるか？→コンパイルエラーとなる
-    int i = 3.2;
+    // 3.2をint型に変換して代入
+    int age = (int)3.2;
+    System.out.println(age);
   }
 }
